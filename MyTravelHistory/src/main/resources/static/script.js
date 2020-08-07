01716b0de7b06ce4546ad7c9402517c685394bdf
AlmostGraduated <plumbumjj@gmail.com>
2020-08-07 17:40:35 +0200

    html and css fixed

--- MyTravelHistory/src/main/resources/static/script.js
+++ MyTravelHistory/src/main/resources/static/script.js
@@ -7,17 +7,6 @@ function registerNewUser(){
     postData(apiURL,newUser);
 }
 
-function showAllRestaurants(){
-    let apiURL = "showAllRestaurants";
-    getData(apiURL);
-}
-
-
-
-
-
-
-
 function postData(apiURL, input){
     let xhttp = new XMLHttpRequest();
     // xhttp.onreadystatechange = function(){
@@ -30,14 +19,46 @@ function postData(apiURL, input){
             xhttp.send(input);
 }
 
-function getData(apiURL){
-    var xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function() {
-              if (this.readyState == 4 && this.status == 200) {
-                document.getElementById("newdiv").innerHTML = this.responseText;
+function showAllRestaurants() {  
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function(){        
+      if (xhr.readyState === XMLHttpRequest.DONE) {
+          if (xhr.status === 'OK' || (xhr.status >= 200 && xhr.status < 400)) {
+              var inhoudDB = JSON.parse(this.responseText);
+              console.log("VERSTUREN GET Restaurants GELUKT!");
+              var restaurantstring = "";
+              for (x=0; x<inhoudDB.length; x++) {
+                  restaurantstring +=
+                  `
+                  <div>${inhoudDB[x].address}</div>
+                  `;
               }
-            };
-    xhttp.open("GET", "http://localhost:8082/"+apiURL);
-              xhttp.setRequestHeader("Content-type", "application/json");
-              xhttp.send();
+              document.getElementById("newdiv").innerHTML = restaurantstring;
+          } else {
+              console.log("VERSTUREN GET EMPLOYEE IS NIET GELUKT!");
+          }
       }
+  }   
+  xhr.open("GET", "http://localhost:8082/showAllRestaurants", true);
+  xhr.send();   
+}
+
+
+
+
+
+// function showAllRestaurants(){
+//     let apiURL = "showAllRestaurants";
+//     getData(apiURL);
+// }
+// function getData(apiURL){
+//     var xhttp = new XMLHttpRequest();
+//             xhttp.onreadystatechange = function() {
+//               if (this.readyState == 4 && this.status == 200) {
+//                 document.getElementById("newdiv").innerHTML = this.responseText;
+//               }
+//             };
+//     xhttp.open("GET", "http://localhost:8082/"+apiURL);
+//               xhttp.setRequestHeader("Content-type", "application/json");
+//               xhttp.send();
+//       }
